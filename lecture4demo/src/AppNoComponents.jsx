73e9d55f86b223d757f261bec58052956d1698a0
Aram Baghdassarian <abagh0703@gmail.com>
2019-03-13 20:24:49 -0400

    more examples

---
+++ lecture4demo/src/AppNoComponents.jsx
@@ -0,0 +1,47 @@
+import React, {Component} from 'react';
+import ContactCard from './ContactCard.jsx';
+import './App.module.css';
+
+class AppNoComponents extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {inputVal: "dummy"};
+    this.changeVal =this.changeVal.bind(this);
+  }
+
+  changeVal(e){
+    this.setState({inputVal: Date.now()});
+  }
+
+    render() {
+    // return (
+    // <ContactCard name={"Justin Tran"} email={"jtt65@cornell.edu"}></ContactCard>
+    // );
+    return (
+      <div>
+        <div className="ContactCard">
+          <h2>
+            Justin Tran
+          </h2>
+          <p>
+            jtt65@cornell.edu
+          </p>
+        </div>
+        <div className="ContactCard">
+          <h2>
+            Aram Baghdassarian
+          </h2>
+          <p>
+            acb352@cornell.edu
+          </p>
+        </div>
+        <div>
+          <input type="text" placeholder="name" value={this.state.inputVal} />
+          <button onClick={this.changeVal} />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default AppNoComponents;
---
+++ lecture4demo/src/ContactCardBasic.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class ContactCardBasic extends Component {
+  render() {
+    return (
+      <div className="contactCardStyle">
+        <h2>
+          {this.props.name}
+        </h2>
+        <p>
+          {this.props.email}
+        </p>
+      </div>
+    );
+  }
+}
+
+export default ContactCardBasic;
