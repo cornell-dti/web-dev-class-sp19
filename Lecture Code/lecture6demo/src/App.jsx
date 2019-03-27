f3b3cb599845e0085aff1034354bd87e098aa105
Sam <sam@developersam.com>
2019-03-27 00:13:30 -0400

    Fix some linter errors in lecture code

--- Lecture Code/lecture6demo/src/App.jsx
+++ Lecture Code/lecture6demo/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import FetchComponent from './FetchComponent.jsx';
+import FetchComponent from './FetchComponent';
 import './App.css';
 
 class App extends Component {
--- Lecture Code/lecture6demo/src/FetchComponent.jsx
+++ Lecture Code/lecture6demo/src/FetchComponent.jsx
@@ -12,28 +12,29 @@ class FetchComponent extends Component {
     //first line does a get request on the api, which returns a PROMISE
     fetch('http://taco-randomizer.herokuapp.com/random/', { method: 'GET' })
 
-    //the promise must be turned into a json
-    .then(response => response.json())
+      // the promise must be turned into a json
+      .then(response => response.json())
 
-    //which can then be added to the state
-    .then(responseJSON => this.setState({ foundTaco: true, randomTaco: responseJSON }));
+      // which can then be added to the state
+      .then(responseJSON => this.setState({ foundTaco: true, randomTaco: responseJSON }));
 
   }
 
   render() {
     const { foundTaco, randomTaco } = this.state;
     return (
-      <div> <h1>Randomly Generated Taco Ingredients</h1>
-      {foundTaco &&
-        <div>
-           <p>Condiment: {randomTaco.condiment.name}</p>
-           <p>Mixin: {randomTaco.mixin.name}</p>
-           <p>Base layer: {randomTaco.base_layer.name}</p>
-           <p>Seasoning: {randomTaco.seasoning.name}</p>
-           <p>Shell: {randomTaco.shell.name}</p>
-        </div>
-      }
-    </div>
+      <div>
+        <h1>Randomly Generated Taco Ingredients</h1>
+        {foundTaco && (
+          <div>
+            <p>Condiment: {randomTaco.condiment.name}</p>
+            <p>Mixin: {randomTaco.mixin.name}</p>
+            <p>Base layer: {randomTaco.base_layer.name}</p>
+            <p>Seasoning: {randomTaco.seasoning.name}</p>
+            <p>Shell: {randomTaco.shell.name}</p>
+          </div>
+        )}
+      </div>
     );
   }
 }
